ed7b7985666b0ce9c8e4c0f0a78745c4375a994c
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-03-20 05:05:31 +0600

    updates

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://'+process.env.user+':'+process.env.pass+'@cluster0.pbwxcxc.mongodb.net/?retryWrites=true&w=majority')
-
+// mongosh "mongodb+srv://cluster0.pbwxcxc.mongodb.net/myFirstDatabase" --apiVersion 1 --username put_user_name
 
 
 
