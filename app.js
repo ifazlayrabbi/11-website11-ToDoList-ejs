7a9444111e1a258fb3630b59791f348d1e24fc29
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-03-20 08:59:24 +0600

    updates

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.static('public'))
 
 require('dotenv').config()
 const _ = require('lodash')
-// const {ObjectId} = require('mongodb')
+const {ObjectId} = require('mongodb')
 // const date = require(__dirname + '/date.js')
 const date = require('./date')
 const {List, Page} = require('./db')
@@ -30,53 +30,7 @@ console.log(today)
 let newItemsArray = []
 const workItemsArray = []
 	
-async function testQuery () {
-	try{
-		
-		// let find1 = await Page.findOne(
-		// 	{'data._id': '6412b33eec028cfd488495fc'},
-		// 	{data: 1, _id: 0}
-		// )
-		// console.log(find1)
 
-
-		// await Page.updateOne(
-		// 	{name: 'Zainab'},
-		// 	{$pull: {data: {name: 'She recites Quran beautifully'}}},
-		// )
-		
-
-		// await Page.updateOne(
-		// 	{name: 'Zainab'},
-		// 	{$pull: {data: {name: 'She recites Quran beautifully', _id: '64149675734c0d9715712b1b'}}}
-		// )
-
-
-
-		// DOESN'T WORK
-		// ------------------------------------------------------------
-		// await Page.updateOne(
-		// 	{name: 'class8'},
-		// 	{$pull: {'students.$[var1].subjects': {subjectName: 'English'}}},
-		// 	{arrayFilters: [{'var1.studentName': 'Kulsoom'}]}
-		// )
-
-		// await Page.updateOne(
-		// 	{name: 'class8'},
-		// 	{$pull: {'students.subjects': {subjectName: 'English'}}}
-		// )
-
-		// await Page.updateOne(
-		// 	{name: 'class8'},
-		// 	{$set: {'students.$[elem].subjects.$[elem2].marks': 60}},
-		// 	{arrayFilters: [{'elem.studentName': 'Kulsoom'},{'elem2.subject': 'English'}]}
-		// )
-		// -----------------------------------------------------------
-
-	
-	} catch (err) {console.log(err.message)}
-}
-// testQuery()
 
 
 
@@ -87,10 +41,10 @@ app.get('/', async (req, res) => {
 		const getItems = await List.find()
 
 		res.render('list', {
-			to_day: today,
+			page_name: today,
 			get_items: getItems,
-			delete_action: '/delete-from-homepage',
-			add_action: '/'
+			// delete_action: '/delete',
+			// add_action: '/'
 		})
 	} catch (err) {
 		res.send('<h1>Error!</h1>' + err.message)
@@ -99,17 +53,20 @@ app.get('/', async (req, res) => {
 })
 
 app.post('/', (req, res) => {
-	let {new_item, page} = req.body
+	let {new_item, page_name} = req.body
 
 	const addItem = new List({
 		name: new_item
 	})
 
-	if(page == today){
+	if(page_name == today){
 		async function func1 (){
 			try{
+				// await List.insertMany({
+				// 	name: new_item
+				// })
 				await addItem.save()
-				console.log('Today data saved.')
+				console.log('Homepage - item saved.')
 				res.redirect('/')
 			} catch (err) {console.log(err.message)}
 		}
@@ -119,11 +76,11 @@ app.post('/', (req, res) => {
 		async function func1(){
 			try{
 				await Page.updateOne(
-					{name: page},
+					{name: page_name},
 					{$push: {data: addItem}}
 				)
-				console.log('Dynamic data saved.')
-				res.redirect('/get/' + page)
+				console.log('Dynamic page - item saved.')
+				res.redirect('/get/' + page_name)
 			} catch (err) {console.log(err.message)}
 		}
 		func1()
@@ -131,33 +88,46 @@ app.post('/', (req, res) => {
 
 })
 
-app.post('/delete-from-homepage', (req, res) => {
-	const {id} = req.body
+app.post('/delete', (req, res) => {
+	const {id, page_name} = req.body
 
-	async function func1(){
-		try{
-			await List.deleteOne({_id: id})
-			console.log('item deleted.')
-		} catch(err) {console.log(err.message)}
+	if(page_name == today){
+		async function func1(){
+			try{
+				// await List.findByIdAndRemove(new ObjectId(id))
+				await List.deleteOne({_id: new ObjectId(id)})
+				console.log('Homepage - item deleted.')
+				res.redirect('/')
+			} catch(err) {console.log(err.message)}
+		}
+		func1()
 	}
-	func1()
-
-	res.redirect('/')
+	else{
+		async function func1(){
+			try{
+				await Page.updateOne(
+					{name: page_name},
+					{$pull: {data: {_id: new ObjectId(id)}}}
+				)
+				console.log('Dynamic page - item deleted.')
+				res.redirect('/get/' + page_name)
+			} catch (err) {console.log(err.message)}
+		}
+		func1()
+	}
 })
 
-let pageName
 app.get('/get/:page', async (req, res) => {
-	pageName = _.capitalize(req.params.page)
+	let pageName = _.capitalize(req.params.page)
 	
 	try{
 		const page = await Page.findOne({name: pageName})
 		if(page){
-			console.log('Requested page exists.')
 			res.render('list', {
-				to_day: pageName,
+				page_name: pageName,
 				get_items: page.data,
-				delete_action: '/delete-from-dynamic',
-				add_action: '/'
+				// delete_action: '/delete',
+				// add_action: '/'
 			})
 		}
 		else{
@@ -180,22 +150,6 @@ app.get('/get/:page', async (req, res) => {
 		console.log(err.message)
 	}
 
-})
-
-app.post('/delete-from-dynamic', (req, res) => {
-	const {id} = req.body
-
-	async function func1(){
-		try{
-			await Page.updateOne(
-				{name: pageName},
-				{$pull: {data: {_id: id}}}
-			)
-			console.log('Deleted from list')
-			res.redirect('/get/' + pageName)
-		} catch (err) {console.log(err.message)}
-	}
-	func1()
 })
 
 
@@ -288,7 +242,53 @@ app.get('/about', (req, res) => res.render('about'))
 
 
 
+async function testQuery () {
+	try{
+		
+		// let find1 = await Page.findOne(
+		// 	{'data._id': '6412b33eec028cfd488495fc'},
+		// 	{data: 1, _id: 0}
+		// )
+		// console.log(find1)
 
+
+		// await Page.updateOne(
+		// 	{name: 'Zainab'},
+		// 	{$pull: {data: {name: 'She recites Quran beautifully'}}},
+		// )
+		
+
+		// await Page.updateOne(
+		// 	{name: 'Zainab'},
+		// 	{$pull: {data: {name: 'She recites Quran beautifully', _id: '64149675734c0d9715712b1b'}}}
+		// )
+
+
+
+		// DOESN'T WORK
+		// ------------------------------------------------------------
+		// await Page.updateOne(
+		// 	{name: 'class8'},
+		// 	{$pull: {'students.$[var1].subjects': {subjectName: 'English'}}},
+		// 	{arrayFilters: [{'var1.studentName': 'Kulsoom'}]}
+		// )
+
+		// await Page.updateOne(
+		// 	{name: 'class8'},
+		// 	{$pull: {'students.subjects': {subjectName: 'English'}}}
+		// )
+
+		// await Page.updateOne(
+		// 	{name: 'class8'},
+		// 	{$set: {'students.$[elem].subjects.$[elem2].marks': 60}},
+		// 	{arrayFilters: [{'elem.studentName': 'Kulsoom'},{'elem2.subject': 'English'}]}
+		// )
+		// -----------------------------------------------------------
+
+	
+	} catch (err) {console.log(err.message)}
+}
+// testQuery()
 
 
 
